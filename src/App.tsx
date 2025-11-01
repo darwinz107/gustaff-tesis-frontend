0a349b3756ba89c252c0d99f56caaed29498300c
darwinz10 <kyosukekanou88@gmail.com>
2025-11-01 00:06:47 -0500

    Pequeños ajustes en ventana principal

--- src/App.tsx
+++ src/App.tsx
@@ -39,16 +39,19 @@ function App() {
       
       
       <div className='flex relative h-screen w-auto bg-pink-200'>
-        <div className='absolute inset-y-0 left-0 h-full w-1/5 z-20 bg-red-200 border-r border-yellow-200'>
-          xd
+        <div className='absolute inset-y-0 left-0 h-full w-1/5 z-20 bg-white border-r border-gray-300 flex flex-col'>
+        <img src="public\gustaff_logo.jpg" className='cursor-pointer mb-7' alt="Gustaff S.A" />
+        <div className='flex flex-col h-full'> 
+           <button className="btn w-full" onClick={soloAdmin}>Solo admin</button>
+      <button className="btn w-full" onClick={()=>navigate('/orden-de-trabajo')}>Orden de trabajo</button>
+      <button className="btn w-full" onClick={logout}>Cerrar sesion</button>
+      </div>
         </div>
         <div className='h-full w-1/5 bg-white'>
           xd
         </div>
-        <div className=' w-4/5 bg-blue-200 h-full'>
-      <button className="btn" onClick={soloAdmin}>Solo admin</button>
-      <button className="btn" onClick={()=>navigate('/orden-de-trabajo')}>Orden de trabajo</button>
-      <button className="btn" onClick={logout}>Cerrar sesion</button></div>
+        <div className=' w-4/5 bg-white h-full'>
+     </div>
 </div>
     </>
   )
