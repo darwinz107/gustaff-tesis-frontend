d2a825bf2408d6c042bdcfd61da513f909015a3e
darwinz10 <kyosukekanou88@gmail.com>
2025-11-05 10:34:25 -0500

    Diseñe el pdf a generar en orden de trabajo

--- src/orden-de-trabajo/view/components/GenerarPdf.tsx
+++ src/orden-de-trabajo/view/components/GenerarPdf.tsx
@@ -1,25 +1,115 @@
 import { Document, Image, Page, PDFViewer, Text, View } from "@react-pdf/renderer"
+import {Table, TR, TH, TD} from '@ag-media/react-pdf-table';
 import { styles } from "../../../styles"
-import logo from "../../../../public/gustaff_logo.jpg"
+import logo from "../../../../public/logo_alternativo.png"
+
 
 
 export const GenerarPdf = () => {
   return (
     <>
-    <PDFViewer width={"100%"} height={"1000vh"}>
-    <Document>
-        <Page size={'A4'} style={styles.page}>
-          <View style={styles.header}>
-            <View><Image src={logo} style={styles.image}></Image></View>
-            <View><Text>GUSTAFF S.A.
-DEPARTAMENTO DE MANTENIMIENTO
-ORDEN DE TRABAJO</Text></View>
-            <View></View>
-            <Text>xdxd</Text>
-          </View>
-        </Page>
-    </Document>
-    </PDFViewer>
+      <PDFViewer width={"100%"} height={"1000vh"}>
+        <Document>
+          <Page size={'A4'} style={styles.page}>
+            <View style={styles.header}>
+              <View style={styles.primero}><Image src={logo} style={styles.image}></Image></View>
+              <View style={styles.segundo}><Text>GUSTAFF S.A.</Text>
+                <Text>DEPARTAMENTO DE MANTENIMIENTO</Text>
+                <Text>ORDEN DE TRABAJO</Text></View>
+              <View style={styles.tercero}>
+                <View style={styles.terceroTexto}><Text>GUMA-RG003</Text></View>
+                <View style={styles.terceroTexto}><Text>REV-002</Text></View>
+                <View ><Text>PAG 1 de 1</Text></View>
+              </View>
+
+            </View>
+            <View style={styles.containerPrincipal}>
+            <View style={styles.container}>
+              <View style={styles.child}>
+                <Text style={styles.childTitulo}>FECHA DE REQUERIMIENTO:</Text>
+                <Text style={styles.childContenido}>01/10/2025</Text>
+                </View>
+                <View style={styles.child}>
+                <Text style={styles.childTitulo}>HORA DE REQUERIMIENTO:</Text>
+                <Text style={styles.childContenido}>01/10/2025</Text>
+                </View>
+                <View style={styles.child}>
+                <Text style={styles.childTitulo}>ÁREA DE TRABAJO</Text>
+                <Text style={styles.childContenido}></Text>
+                </View>
+            </View>
+             <View style={styles.container}>
+              <View style={styles.child}>
+                <Text style={styles.childTitulo}>FECHA DE INICIO PLANIFICADA:</Text>
+                <Text style={styles.childContenido}>10:51:34</Text>
+                </View>
+                <View style={styles.child}>
+                <Text style={styles.childTitulo}>HORA DE REQUERIMIENTO:</Text>
+                <Text style={styles.childContenido}>10:51:34</Text>
+                </View>
+                <View style={styles.child}>
+                <View style={styles.orden}>
+                  <Text >Orden:    </Text>
+                <Text >OTM00634</Text>
+                </View>
+                </View>
+            </View>
+          
+            </View>
+                 <View style={styles.container}>
+             <Table style={styles.table}>
+              <TH style={styles.th}>
+                <TD style={styles.td}>CATEGORIA</TD>
+                <TD style={styles.td}>TIPO DE TRABAJO</TD>
+              </TH>
+              <TR style={styles.tr}>
+                <TD style={styles.td}>MECÁNICA</TD>
+                <TD style={styles.td}>FABRICACION</TD>
+              </TR>
+             </Table>
+            </View>
+                 <View style={styles.container}>
+             <Table style={styles.table}>
+              <TH style={styles.th}>
+                <TD style={styles.td}>CODIGO</TD>
+                <TD style={styles.td}>NOMBRE DE MÁQUINA, EQUIPO O PIEZA</TD>
+              </TH>
+              <TR style={styles.tr}>
+                <TD style={styles.td}>N/A</TD>
+                <TD style={styles.td}>TEMPERADORA NUEVA</TD>
+              </TR>
+             </Table>
+            </View>
+               <View style={styles.container}>
+             <Table style={styles.table}>
+              <TH style={styles.th}>
+                <TD style={styles.td}>DESCRIPCIÓN DE TRABAJO</TD>
+                
+              </TH>
+              <TR style={styles.tr}>
+                <TD style={styles.textArea}>FABRICACION Y HABILITACION DE SISTEMAS DE AGUA PARA LA TEMPERADORA 
+</TD>                
+              </TR>
+             </Table>
+            </View>
+             <View style={styles.containerFirmas}>
+              <View style={styles.childFirma}>  
+                <Text style={styles.childContenido}>Darwin Zambrano</Text>
+                <Text >SOLICITA</Text>              
+                </View> 
+                <View style={styles.childFirma}>  
+                <Text style={styles.childContenido}>   </Text>
+                <Text >COOR. DE MANTENIMIENTO</Text> 
+                <Text>VERIFICACIÓN</Text>             
+                </View>
+                <View style={styles.childFirma}>  
+                <Text style={styles.childContenido}>Jhonson Cando</Text>
+                <Text >RECIBE</Text>              
+                </View>              
+            </View>
+          </Page>
+        </Document>
+      </PDFViewer>
     </>
   )
 }
--- src/styles.ts
+++ src/styles.ts
@@ -5,7 +5,7 @@ import { table } from "console";
 export const styles = StyleSheet.create({
     page:{
       backgroundColor:"#ffffffff",
-    
+      fontSize:"8px"
     },
 
     principal:{
@@ -14,15 +14,106 @@ export const styles = StyleSheet.create({
     },
     header:{
         margin:"10px",
-        width:"95%",
+        width:"97%",
         border:"1px solid black",
-        display:"flex"
+        display:"flex",
+        flexDirection:"row",     
+        alignItems:"center",
+        fontWeight:"bold"
     },
    
     table:{
 
     },
     image:{
-      width:"100px"
+      width:"60px",
+      margin:"5px"
     }
+,
+    primero:{
+      width:"15%",
+      
+    },
+    segundo:{
+     width:"80%",
+     height:"100%",
+     display:"flex",
+     flexDirection:"column",
+     textAlign:"center",
+     borderLeft:"1px black solid",
+     borderRight:"1px black solid",
+     justifyContent:"center"
+    },
+    tercero:{
+     width:"20%",
+     textAlign:"center",
+     justifyContent:"center"
+    },
+    terceroTexto:{
+      borderBottom:"1px black solid"
+    },
+    containerPrincipal:{
+      display:"flex",
+      flexDirection:"row"
+    },
+    container:{
+      marginHorizontal:"5px",
+      marginVertical:"10px"
+      
+    },
+    child:{
+      display:"flex",
+      flexDirection:"row",
+      marginTop:"9px"
+    },
+
+    childTitulo:{
+      marginRight:"15px"
+    },
+    childContenido:{
+      borderBottom:"1px solid black",
+      paddingHorizontal:"50px"
+    },
+    orden:{
+      fontWeight:"bold",
+      display:"flex",
+      flexDirection:"row"
+    },
+    table:{
+     
+    },
+    th:{
+     textAlign:"center",
+     backgroundColor:"#bdbdbdff"
+    }
+    ,
+    tr:{
+
+    }
+    ,
+    td:{
+      padding:"2px",
+      justifyContent:"center"
+    },
+    textArea:{
+      minHeight:"50%",
+      alignItems:"flex-start",
+      padding:"2px"
+    }
+    ,
+    containerFirmas:{
+      position:"absolute",
+      top:"47%",
+      left:"2%",
+      display:"flex",
+      flexDirection:"row",
+      width:"97%",
+      justifyContent:"space-between"
+    },
+    childFirma:{
+      display:"flex",
+      flexDirection:"column",
+      alignItems:"center"
+    }
+    
 })
